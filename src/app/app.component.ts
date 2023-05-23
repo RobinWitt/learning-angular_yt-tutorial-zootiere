4a05d2c352f7bcd01db20e95e6239cc420a6fe57
RobinWitt <reg.rw@posteo.de>
2023-05-23 14:48:01 +0200

    finish tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'youtube-beginners-tutorial_zoo';
+  title = 'Zootiere';
+  animals: Animal[] = [
+    {
+      name: 'Jürgen',
+      type: 'Affe',
+    },
+    {
+      name: 'Tina',
+      type: 'Schildkröte',
+    },
+    {
+      name: 'Tim',
+      type: 'Schnabeltier',
+    },
+    {
+      name: 'Luise',
+      type: 'Zebra',
+    },
+  ];
+
+  newAnimal: Animal = {
+    name: 'New Name',
+    type: 'New Type',
+  };
+
+  addAnimal() {
+    this.animals.push(this.newAnimal);
+  }
+
+  deleteAnimal(animal: Animal): void {
+    this.animals = this.animals.filter((a) => a != animal);
+  }
 }
